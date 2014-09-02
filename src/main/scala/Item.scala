0dc1dda7fd081060d0dde5c4582dc09a4a8df6b8
xuwei-k <6b656e6a69@gmail.com>
2014-09-02 22:47:13 +0900

    replace wasted spaces and tag

--- src/main/scala/Item.scala
+++ src/main/scala/Item.scala
@@ -10,7 +10,7 @@ final case class Item private(
   def tweetString(hashTags: Set[String] = Set.empty): String = {
     val tags = hashTags.collect{case s if ! s.isEmpty => "#" + s }.mkString(" ")
     Iterator(
-      link, tags, escape(title), escape(description)
+      link, tags, escape(title), escape(description.replace("            <p>", ""))
     ).mkString("\n").take(LIMIT)
   }
 }
